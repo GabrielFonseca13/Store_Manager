1512f305bc60c4fb22fb5e1b3b3328f78c79ad63
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-03-24 13:35:48 -0300

    teste

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,7 +4,7 @@ const errorMap = require('../utils/errorMap');
 const listProducts = async (_req, res) => {
   const { type, message } = await productService.findAll();
 
-  // if (type) return res.status(errorMap.mapError(type)).json(message);
+  if (type) return res.status(errorMap.mapError(type)).json(message);
 
   res.status(200).json(message);
 };
