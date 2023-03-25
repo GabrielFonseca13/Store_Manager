cefdbef14f6133ae9ff2f09582533584d05a251a
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-03-24 21:23:49 -0300

    fix req1

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,7 +13,7 @@ const getProduct = async (req, res) => {
   const { id } = req.params;
   const { type, message } = await productService.findById(id);
 
-  if (type) return res.status(errorMap.mapError(type)).json(message);
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
 
   res.status(200).json(message);
 };
