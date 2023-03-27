d5c74e21886988ddcaae574b663f839c42860116
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-03-27 08:40:20 -0300

    fix lint

--- src/middlewares/validatenewSaleFields.js
+++ src/middlewares/validatenewSaleFields.js
@@ -4,7 +4,9 @@ module.exports = (req, res, next) => {
   ItemsSold.forEach((itemSold) => {
   if (!itemSold.productId) return res.status(400).json({ message: '"productId" is required' });
 
-  if (!itemSold.quantity && itemSold.quantity !== 0) return res.status(400).json({ message: '"quantity" is required' });
+    if (!itemSold.quantity && itemSold.quantity !== 0) {
+      return res.status(400).json({ message: '"quantity" is required' });
+    }
   });
 
   return next();
